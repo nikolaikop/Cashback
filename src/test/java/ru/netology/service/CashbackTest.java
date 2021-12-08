409e757988a5174f265a4f79896e3a05618b4d2e
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-12-08 14:03:20 -0700

    junit4-platform

--- src/test/java/ru/netology/service/CashbackTest.java
+++ src/test/java/ru/netology/service/CashbackTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.*;
 
@@ -17,9 +18,22 @@ public class CashbackTest {
     }
 
     @Test
-    public void shouldReturn200() {
+    public void shouldReturn300() {
         int actual = service.remain(700);
         int expected = 300;
         Assert.assertEquals(expected,actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldReturnNullV2() {
+        int actual = service.remain(1000);
+        Assertions.assertEquals(0, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldReturn300v2() {
+        int actual = service.remain(700);
+        Assertions.assertEquals(300, actual);
+    }
+
 }
